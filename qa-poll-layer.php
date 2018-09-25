d95632870ec3ccab4558924d803cd43a32dd672e
Iran Samaneh <iransamaneh@users.noreply.github.com>
2018-09-25 06:06:57 +0000

    fix unvote javascript bug

--- qa-poll-layer.php
+++ qa-poll-layer.php
@@ -295,7 +295,7 @@ function pollVote(qid,uid,vid,cancel) {
 				else if(!in_array($uid,$votes))
 					$answers[$idx]['vote'] = '<div class="qa-poll-vote-button" title="'.qa_html(qa_lang('polls/vote_button')).'" onclick="pollVote('.$qid.',\''.$uid.'\','.$answer['id'].')"></div>';
 				else {
-					$answers[$idx]['vote'] = '<div class="qa-poll-voted-button" title="'.qa_html(qa_lang('polls/voted_button')).'" onclick="pollVote('.$qid.',\''.$uid.'\',\''.$answer['id'].',1)"></div>';
+					$answers[$idx]['vote'] = '<div class="qa-poll-voted-button" title="'.qa_html(qa_lang('polls/voted_button')).'" onclick="pollVote('.$qid.',\''.$uid.'\','.$answer['id'].',1)"></div>';
 					
 					$voted = true;
 				}
